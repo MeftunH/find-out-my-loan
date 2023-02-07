0b0880b0da541707c6a5007c2d8c91f6954fcaa2
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-07 22:12:03 +0300

    Initial commit

---
+++ src/main/java/com/example/findoutmycreditscore/FindOutMyCreditScoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.findoutmycreditscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindOutMyCreditScoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindOutMyCreditScoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/findoutmycreditscore/FindOutMyCreditScoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.findoutmycreditscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindOutMyCreditScoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
