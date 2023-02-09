8e244bb7db27591cd324aa3d48b88a1ec2361e5a
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-09 07:10:40 +0300

    FEAT: add customer

---
+++ src/main/java/com/findoutmycreditscore/application/customer/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.findoutmycreditscore.application.customer.repository;
+/* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
+
+import com.findoutmycreditscore.application.person.repository.PersonRepository;
+
+public interface CustomerRepository extends PersonRepository {
+}
