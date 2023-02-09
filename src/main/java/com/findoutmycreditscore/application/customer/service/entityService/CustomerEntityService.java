a025c90688462f2745b0026a55d9c3ac6c5ddbe8
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-09 11:26:06 +0300

    FEAT: add entity service

---
+++ src/main/java/com/findoutmycreditscore/application/customer/service/entityService/CustomerEntityService.java
@@ -0,0 +1,5 @@
+package com.findoutmycreditscore.application.customer.service.entityService;
+/* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
+
+public interface CustomerEntityService {
+}
