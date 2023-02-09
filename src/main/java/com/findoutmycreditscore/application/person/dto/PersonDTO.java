d9225ba4e7411eb2d007d346dbae553aa49dfe74
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-09 11:23:37 +0300

    REF: add person dto

---
+++ src/main/java/com/findoutmycreditscore/application/person/dto/PersonDTO.java
@@ -0,0 +1,22 @@
+package com.findoutmycreditscore.application.person.dto;
+/* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
+
+import com.findoutmycreditscore.application.person.enums.PersonType;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.util.Date;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+public class PersonDTO {
+    private String name;
+    private String surname;
+    private long identityNo;
+    private Date birthDate;
+    private String phoneNumber;
+    private PersonType personType;
+
+}
