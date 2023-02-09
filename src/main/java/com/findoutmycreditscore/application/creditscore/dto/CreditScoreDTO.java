b1941fe3448a2cfe12e2ef79283a80503073040f
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-09 03:21:11 +0300

    FEAT: add credit score dto

---
+++ src/main/java/com/findoutmycreditscore/application/creditscore/dto/CreditScoreDTO.java
@@ -0,0 +1,10 @@
+package com.findoutmycreditscore.application.creditscore.dto;
+/* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
+
+import lombok.Data;
+
+@Data
+public class CreditScoreDTO {
+    private int creditScore;
+
+}
