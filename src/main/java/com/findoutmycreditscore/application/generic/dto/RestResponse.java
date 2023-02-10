08b20afe155271efcf5934c4acf0aabbb4dc0e0d
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-11 01:10:50 +0300

    FEAT: add rest response

---
+++ src/main/java/com/findoutmycreditscore/application/generic/dto/RestResponse.java
@@ -0,0 +1,39 @@
+package com.findoutmycreditscore.application.generic.dto;
+/* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RestResponse<T> implements Serializable {
+    private T data;
+    private Date responseDate;
+    private boolean isSuccess;
+    private String message;
+
+    public RestResponse(T data, boolean isSuccess) {
+        this.data=data;
+        this.isSuccess=isSuccess;
+        responseDate=new Date();
+    }
+
+    public static <T> RestResponse<T> of(T t){
+        return new RestResponse<>(t,true);
+    }
+    public static <T> RestResponse<T> error(T t){
+        return new RestResponse<>(t,false);
+    }
+    public static <T> RestResponse<T> empty(T t){
+        return new RestResponse<>(null,false);
+    }
+
+    public void setMessage(String message) {
+        this.message=message;
+    }
+}
