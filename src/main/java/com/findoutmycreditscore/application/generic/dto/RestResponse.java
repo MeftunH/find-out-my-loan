c206b02e4e87f8987b56410c5692d3d1695cc5bf
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-11 01:12:04 +0300

    FIX: rest response

--- src/main/java/com/findoutmycreditscore/application/generic/dto/RestResponse.java
+++ src/main/java/com/findoutmycreditscore/application/generic/dto/RestResponse.java
@@ -15,25 +15,32 @@ public class RestResponse<T> implements Serializable {
     private T data;
     private Date responseDate;
     private boolean isSuccess;
-    private String message;
+    private String messages;
 
     public RestResponse(T data, boolean isSuccess) {
-        this.data=data;
-        this.isSuccess=isSuccess;
-        responseDate=new Date();
+        this.data = data;
+        this.isSuccess = isSuccess;
+        responseDate = new Date();
     }
 
     public static <T> RestResponse<T> of(T t){
-        return new RestResponse<>(t,true);
-    }
-    public static <T> RestResponse<T> error(T t){
-        return new RestResponse<>(t,false);
-    }
-    public static <T> RestResponse<T> empty(T t){
-        return new RestResponse<>(null,false);
+        return new RestResponse<>(t, true);
     }
 
-    public void setMessage(String message) {
-        this.message=message;
+    public static <T> RestResponse<T> error(T t){
+        return new RestResponse<>(t, false);
     }
+
+    public static <T> RestResponse<T> empty(){
+        return new RestResponse<>(null, true);
+    }
+
+    public void setMessages(String messages) {
+        this.messages = messages;
+    }
+
+    public String getMessage(){
+        return messages;
+    }
+
 }
