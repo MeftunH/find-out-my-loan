b7fec73d0c44aac42e52d4d52919f5dd4608aae7
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-10 03:14:42 +0300

    FEAT: add save customer functionality

--- src/main/java/com/findoutmycreditscore/application/customer/service/CustomerService.java
+++ src/main/java/com/findoutmycreditscore/application/customer/service/CustomerService.java
@@ -1,5 +1,9 @@
 package com.findoutmycreditscore.application.customer.service;
 /* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
 
+import com.findoutmycreditscore.application.customer.dto.CustomerDTO;
+import com.findoutmycreditscore.application.customer.dto.CustomerSaveRequestDTO;
+
 public interface CustomerService {
+    CustomerDTO saveCustomer(CustomerSaveRequestDTO customerSaveRequestDTO);
 }
