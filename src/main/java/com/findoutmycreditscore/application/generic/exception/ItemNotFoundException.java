5870b8495f7bb29cf2cfd0f42479649cd0201d9a
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-11 23:11:41 +0300

    REF: extends gen business exception

--- src/main/java/com/findoutmycreditscore/application/generic/exception/ItemNotFoundException.java
+++ src/main/java/com/findoutmycreditscore/application/generic/exception/ItemNotFoundException.java
@@ -1,14 +1,14 @@
 package com.findoutmycreditscore.application.generic.exception;
 /* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
 
+import com.findoutmycreditscore.application.generic.dto.RestResponse;
 import com.findoutmycreditscore.application.generic.errorMessage.BaseErrorMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class ItemNotFoundException extends RuntimeException {
+public class ItemNotFoundException extends GenericBusinessException {
     public ItemNotFoundException(BaseErrorMessage message) {
-        super(message.getErrorMessage());
+        super(message);
     }
-
 }
