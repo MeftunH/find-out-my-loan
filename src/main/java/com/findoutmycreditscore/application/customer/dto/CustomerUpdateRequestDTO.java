aece29f63b126f111f63fe11312e00b9c4eab2f2
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-12 23:02:53 +0300

    REF: add validator annotations

--- src/main/java/com/findoutmycreditscore/application/customer/dto/CustomerUpdateRequestDTO.java
+++ src/main/java/com/findoutmycreditscore/application/customer/dto/CustomerUpdateRequestDTO.java
@@ -2,6 +2,7 @@ package com.findoutmycreditscore.application.customer.dto;
 
 import com.findoutmycreditscore.application.credit.enums.PaybackGuaranteeType;
 import com.findoutmycreditscore.application.person.dto.PersonDTO;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -13,8 +14,9 @@ import java.io.Serializable;
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class CustomerUpdateRequestDTO extends PersonDTO implements Serializable {
+    @NotNull
     private final float monthlyIncome;
-
+    @NotNull
     private final float customerLimit;
     private final String accountPassword;
 }
