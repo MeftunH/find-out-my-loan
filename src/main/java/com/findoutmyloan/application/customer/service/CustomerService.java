575bf343dd813c9380aa1e8c928ac449c3265cac
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-14 22:14:36 +0300

    REF: add getCustomerTypeAccordingToMonthlyIncome functionality

--- src/main/java/com/findoutmyloan/application/customer/service/CustomerService.java
+++ src/main/java/com/findoutmyloan/application/customer/service/CustomerService.java
@@ -4,13 +4,16 @@ package com.findoutmyloan.application.customer.service;
 import com.findoutmyloan.application.customer.dto.CustomerDTO;
 import com.findoutmyloan.application.customer.dto.CustomerSaveRequestDTO;
 import com.findoutmyloan.application.customer.dto.CustomerUpdateRequestDTO;
+import com.findoutmyloan.application.customer.entity.Customer;
+import com.findoutmyloan.application.customer.enums.CustomerTypeAccordingToMonthlyIncome;
 
 public interface CustomerService {
     CustomerDTO saveCustomer(CustomerSaveRequestDTO customerSaveRequestDTO);
 
     CustomerDTO getByIdWithControl(Long id);
+    Customer findCustomerByIdentityNoOrThrowException(Long id);
 
     void deleteCustomerByIdWithControl(Long id);
-
+    CustomerTypeAccordingToMonthlyIncome getCustomerTypeAccordingToMonthlyIncome(float monthlyIncome);
     CustomerDTO updateCustomer(CustomerUpdateRequestDTO customerUpdateRequestDTO);
 }
