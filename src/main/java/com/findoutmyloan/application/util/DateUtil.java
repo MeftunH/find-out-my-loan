8cbd0898f2cd9210ff0ba2380847ca39abbfdb61
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-16 10:58:57 +0300

    REF: Date util validation

--- src/main/java/com/findoutmyloan/application/util/DateUtil.java
+++ src/main/java/com/findoutmyloan/application/util/DateUtil.java
@@ -11,17 +11,20 @@ import java.util.Date;
 
 public class DateUtil {
     public static LocalDate convertToDateTime(Date dateToConvert) {
-        validateDate(dateToConvert);
-        return dateToConvert.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        if(dateToConvert == null)
+            throw new GeneralBusinessException(GeneralErrorMessage.DATE_COULD_NOT_BE_CONVERTED);
+        return dateToConvert.toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDate();
     }
-
     public static Date convertToDate(LocalDate dateToConvert) {
-        validateDate(convertToDate(dateToConvert));
-        return Date.from(dateToConvert.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+        if(dateToConvert == null)
+            throw new GeneralBusinessException(GeneralErrorMessage.DATE_COULD_NOT_BE_CONVERTED);
+        return Date
+                .from(dateToConvert.atStartOfDay()
+                        .atZone(ZoneId.systemDefault())
+                        .toInstant());
     }
 
-    private static void validateDate(Date date) {
-        if (date==null) throw new GeneralBusinessException(GeneralErrorMessage.DATE_COULD_NOT_BE_CONVERTED);
-    }
 
 }
