30efd26ea5461c23fb1bc32e56f4d9c3e7281eb7
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-16 04:38:05 +0300

    FEAT: add get current customer

--- src/main/java/com/findoutmyloan/application/security/service/AuthenticationService.java
+++ src/main/java/com/findoutmyloan/application/security/service/AuthenticationService.java
@@ -3,10 +3,13 @@ package com.findoutmyloan.application.security.service;
 
 import com.findoutmyloan.application.customer.dto.CustomerDTO;
 import com.findoutmyloan.application.customer.dto.CustomerSaveRequestDTO;
+import com.findoutmyloan.application.customer.entity.Customer;
+import com.findoutmyloan.application.customer.mapper.CustomerMapper;
 import com.findoutmyloan.application.customer.service.CustomerService;
 import com.findoutmyloan.application.security.dto.SecurityLoginRequestDTO;
 import com.findoutmyloan.application.security.enums.JwtConstant;
 import com.findoutmyloan.application.security.jwt.JwtTokenGenerator;
+import com.findoutmyloan.application.security.jwt.JwtUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -23,6 +26,14 @@ public class AuthenticationService {
     private final AuthenticationManager authenticationManager;
     private final JwtTokenGenerator jwtTokenGenerator;
 
+    private static JwtUserDetails getCurrentJwtUserDetails(Authentication authentication) {
+        JwtUserDetails jwtUserDetails=null;
+        if (authentication!=null&&authentication.getPrincipal() instanceof JwtUserDetails) {
+            jwtUserDetails=(JwtUserDetails) authentication.getPrincipal();
+        }
+        return jwtUserDetails;
+    }
+
     public CustomerDTO register(CustomerSaveRequestDTO customerSaveRequestDTO) {
         return customerService.saveCustomer(customerSaveRequestDTO);
     }
@@ -33,5 +44,18 @@ public class AuthenticationService {
         SecurityContextHolder.getContext().setAuthentication(authentication);
         String token=jwtTokenGenerator.generateJwtToken(authentication);
         return JwtConstant.BEARER.getConstant()+token;
+    }
+
+
+
+    public Customer getCurrentCustomer() {
+        Authentication authentication=SecurityContextHolder.getContext().getAuthentication();
+        CustomerDTO customerDTO=null;
+        JwtUserDetails jwtUserDetails=jwtUserDetails=getCurrentJwtUserDetails(authentication);
+        if (jwtUserDetails==null) {
+            customerDTO=customerService.getByIdWithControl(jwtUserDetails.getId());
+        }
+
+        return CustomerMapper.INSTANCE.convertToCustomer(customerDTO);
     }
 }
