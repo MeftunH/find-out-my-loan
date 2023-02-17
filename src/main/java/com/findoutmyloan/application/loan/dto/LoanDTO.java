954958e42628849a21d237b8d428dcac69e67fa7
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-17 13:27:35 +0300

    REF: set builder pattern

--- src/main/java/com/findoutmyloan/application/loan/dto/LoanDTO.java
+++ src/main/java/com/findoutmyloan/application/loan/dto/LoanDTO.java
@@ -3,6 +3,7 @@ package com.findoutmyloan.application.loan.dto;
 import com.findoutmyloan.application.loan.entity.Loan;
 import com.findoutmyloan.application.loan.enums.LoanResult;
 import com.findoutmyloan.application.loan.enums.PaybackGuaranteeType;
+import lombok.Builder;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -12,6 +13,7 @@ import java.util.Date;
  * A DTO for the {@link Loan} entity
  */
 @Data
+@Builder
 public class LoanDTO implements Serializable {
     private final PaybackGuaranteeType paybackGuaranteeType;
     private float amount;
