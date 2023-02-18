cf7feaeb639ec69089e0cec98eb4819dcab7d731
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-18 03:02:42 +0300

    REF: Impl methods

--- src/main/java/com/findoutmyloan/application/surety/validation/impl/SuretyValidationServiceImpl.java
+++ src/main/java/com/findoutmyloan/application/surety/validation/impl/SuretyValidationServiceImpl.java
@@ -1,7 +1,26 @@
 package com.findoutmyloan.application.surety.validation.impl;
 /* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
 
+import com.findoutmyloan.application.general.exception.IllegalFieldException;
+import com.findoutmyloan.application.person.enums.PersonType;
+import com.findoutmyloan.application.surety.entity.Surety;
+import com.findoutmyloan.application.surety.enums.SuretyErrorMessage;
 import com.findoutmyloan.application.surety.validation.SuretyValidationService;
 
 public class SuretyValidationServiceImpl implements SuretyValidationService {
+    @Override
+    public void validateIsPersonTypeSurety(Surety surety) {
+        if (!surety.getPersonType().equals(PersonType.SURETY))
+            throw new IllegalFieldException(SuretyErrorMessage.PERSON_TYPE_MUST_BE_SURETY)
+    }
+
+    @Override
+    public void validateSuretyType(Surety surety) {
+
+    }
+
+    @Override
+    public void validateAreFieldsNull(String suretyType) {
+
+    }
 }
