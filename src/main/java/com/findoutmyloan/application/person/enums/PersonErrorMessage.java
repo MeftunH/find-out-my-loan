443d2d3363be5e963a1aae3011630dcac606b17c
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-18 23:19:04 +0300

    FIX: add error messages

--- src/main/java/com/findoutmyloan/application/person/enums/PersonErrorMessage.java
+++ src/main/java/com/findoutmyloan/application/person/enums/PersonErrorMessage.java
@@ -4,13 +4,9 @@ package com.findoutmyloan.application.person.enums;
 import com.findoutmyloan.application.general.errorMessage.BaseErrorMessage;
 
 public enum PersonErrorMessage implements BaseErrorMessage {
-    IDENTITY_NO_MUST_BE_UNIQUE("Identity No Must Be Unique!","Please check the identity no of the customer."),
-    PHONE_NUMBER_INVALID(  "Phone Number Invalid!","Please check the phone number." ),
-    PHONE_NUMBER_MUST_BE_UNIQUE("Phone Number Must Be Unique!","Please check the phone number."),
-    FIELD_CANNOT_BE_NULL("Field Cannot Be Null!","Please check the field."),
-    IDENTITY_NO_INVALID("Identity No Invalid!","Please check the identity no."),
-    BIRTH_DATE_INVALID("Birth Date Invalid!","Please check the birth date.")
-    ;
+
+    FIELD_CANNOT_BE_NULL("Field Cannot Be Null!","Please check the field.");
+
 
     private  final String message;
     private  final String detailMessage;
