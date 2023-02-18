1326de83d1ef103fc8662c3e685f864482038cd6
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-18 23:20:04 +0300

    FIX: add error messages

--- src/main/java/com/findoutmyloan/application/customer/enums/CustomerErrorMessage.java
+++ src/main/java/com/findoutmyloan/application/customer/enums/CustomerErrorMessage.java
@@ -5,11 +5,17 @@ import com.findoutmyloan.application.general.errorMessage.BaseErrorMessage;
 
 public enum CustomerErrorMessage implements BaseErrorMessage {
     CUSTOMER_NOT_FOUND("Customer Not Found!","Please check the id of the customer."),
-    FIELD_CANNOT_BE_NULL("Field Cannot Be Null!","Please be sure that all fields are entered."),
-    INFORMATION_MISMATCH("Information Mismatch!","Please make sure you have entered your information correctly."),
+    FIELD_CANNOT_BE_NULL("Customer Field Cannot Be Null!","Please be sure that all fields are entered."),
+    INFORMATION_MISMATCH("Customer Information Mismatch!","Please make sure you have entered your information correctly."),
     MONTHLY_INCOME_CANNOT_BE_NEGATIVE( "Monthly Income Cannot Be Negative!","Please check the monthly income." ),
     PASSWORD_MUST_BE_AT_LEAST_THREE_CHARACTERS( "Password Must Be At Least Three Characters!","Please check the password."),
-    PERSON_TYPE_MUST_BE_CUSTOMER("Person Type Must Be Customer!","Please check the person type.");
+    PERSON_TYPE_MUST_BE_CUSTOMER("Person Type Must Be Customer!","Please check the person type."),
+    CUSTOMER_IDENTITY_NO_MUST_BE_UNIQUE("Customer Identity No Must Be Unique!","Please check the identity no of the customer."),
+    CUSTOMER_PHONE_NUMBER_INVALID(  "Customer Phone Number Invalid!","Please check the phone number." ),
+    CUSTOMER_PHONE_NUMBER_MUST_BE_UNIQUE("Customer Phone Number Must Be Unique!","Please check the phone number."),
+    CUSTOMER_IDENTITY_NO_INVALID("Customer Identity No Invalid!","Please check the identity no."),
+    CUSTOMER_BIRTH_DATE_INVALID("Customer Birth Date Invalid!","Please check the birth date.")
+            ;
 
     private  final String message;
     private  final String detailMessage;
