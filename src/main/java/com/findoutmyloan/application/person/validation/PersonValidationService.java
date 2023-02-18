c2dca7f91a1a58cb78ac6668c14b4f9e7cd19238
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-18 23:18:14 +0300

    FIX: person validation service

--- src/main/java/com/findoutmyloan/application/person/validation/PersonValidationService.java
+++ src/main/java/com/findoutmyloan/application/person/validation/PersonValidationService.java
@@ -1,14 +1,15 @@
 package com.findoutmyloan.application.person.validation;
 /* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
 
+import com.findoutmyloan.application.general.errorMessage.BaseErrorMessage;
 import com.findoutmyloan.application.person.entity.Person;
 
 import java.util.Date;
 
 public interface PersonValidationService {
-    void validatePhoneNumber(String phoneNumber);
-    void validateIsIdentityNoUnique(Person person);
-    void validateIsPhoneNoUnique(Person person);
-    void validateTurkishIdentityNo(long identityNo);
-    void validateBirthDate(Date birthDate);
+    void validatePhoneNumber(String phoneNumber, BaseErrorMessage baseErrorMessage);
+    void validateIsIdentityNoUnique(Person person, BaseErrorMessage baseErrorMessage);
+    void validateIsPhoneNoUnique(Person person, BaseErrorMessage baseErrorMessage);
+    void validateTurkishIdentityNo(long identityNo, BaseErrorMessage baseErrorMessage);
+    void validateBirthDate(Date birthDate, BaseErrorMessage baseErrorMessage);
 }
