4f42a31d791c067b6bba0dd45a0cd9fc06377703
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-19 05:24:28 +0300

    REF: rebase dto

--- src/main/java/com/findoutmyloan/application/customer/service/CustomerService.java
+++ src/main/java/com/findoutmyloan/application/customer/service/CustomerService.java
@@ -7,10 +7,9 @@ import com.findoutmyloan.application.customer.dto.CustomerSaveRequestDTO;
 import com.findoutmyloan.application.customer.dto.CustomerUpdateRequestDTO;
 import com.findoutmyloan.application.customer.entity.Customer;
 import com.findoutmyloan.application.customer.enums.CustomerTypeAccordingToMonthlyIncome;
-import com.findoutmyloan.application.loan.dto.LoanApplicationRequestDTO;
+import com.findoutmyloan.application.facade.dto.LoanApplicationRequestDTO;
 import com.findoutmyloan.application.loan.dto.LoanDTO;
 
-import java.security.GeneralSecurityException;
 import java.util.Date;
 import java.util.List;
 
