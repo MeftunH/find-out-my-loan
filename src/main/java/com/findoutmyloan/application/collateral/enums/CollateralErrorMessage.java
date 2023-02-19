ee875bfb93b078c43b90b48b14e0414d34b6a709
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-19 10:17:01 +0300

    REF: collateral module refactor

--- src/main/java/com/findoutmyloan/application/collateral/enums/CollateralErrorMessage.java
+++ src/main/java/com/findoutmyloan/application/collateral/enums/CollateralErrorMessage.java
@@ -5,7 +5,7 @@ import com.findoutmyloan.application.general.errorMessage.BaseErrorMessage;
 
 public enum CollateralErrorMessage implements BaseErrorMessage {
     WORT_MUST_BE_POSITIVE("Worth must be positive", "Please enter a positive value for worth"),
-    FIELD_CANNOT_BE_NULL("Field cannot be null", "Please check the fields");
+    FIELD_CANNOT_BE_NULL("Collateral Fields cannot be null", "Please check the fields");
    private final String message;
     private final String detailMessage;
 
