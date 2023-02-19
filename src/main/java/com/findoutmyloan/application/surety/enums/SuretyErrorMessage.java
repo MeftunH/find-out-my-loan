ce9ae57f26881880f26e94f78109d1df2478c9d6
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-19 05:24:49 +0300

    REF: add not null validation

--- src/main/java/com/findoutmyloan/application/surety/enums/SuretyErrorMessage.java
+++ src/main/java/com/findoutmyloan/application/surety/enums/SuretyErrorMessage.java
@@ -11,8 +11,8 @@ public enum SuretyErrorMessage  implements BaseErrorMessage {
     SURETY_PHONE_NUMBER_INVALID(  "Surety Phone Number Invalid!","Please check the phone number." ),
     SURETY_PHONE_NUMBER_MUST_BE_UNIQUE("Surety Phone Number Must Be Unique!","Please check the phone number."),
     SURETY_IDENTITY_NO_INVALID("Surety Identity No Invalid!","Please check the identity no."),
-    SURETY_BIRTH_DATE_INVALID("Surety Birth Date Invalid!","Please check the birth date.")
-            ;
+    SURETY_BIRTH_DATE_INVALID("Surety Birth Date Invalid!","Please check the birth date."),
+    SURETY_FIELD_CANNOT_BE_NULL("Field cannot be null", "Please check the fields" );
     private  final String message;
     private  final String detailMessage;
     SuretyErrorMessage(String message, String detailMessage){
--- src/main/java/com/findoutmyloan/application/surety/validation/SuretyValidationService.java
+++ src/main/java/com/findoutmyloan/application/surety/validation/SuretyValidationService.java
@@ -1,10 +1,12 @@
 package com.findoutmyloan.application.surety.validation;
 /* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
 
+import com.findoutmyloan.application.surety.dto.SuretySaveRequestDTO;
 import com.findoutmyloan.application.surety.entity.Surety;
 
 public interface SuretyValidationService {
     void validateIsPersonTypeSurety(Surety surety);
     void validateSuretyType(Surety surety);
+    void validateFieldsAreNotNull(Surety surety);
     void validateSurety(Surety surety);
 }
