e087d0d23df3567c7241202331de06118cefb66a
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-20 08:00:08 +0300

    TEST: add shouldGetUpdatedLimitOfCustomer test

--- src/test/java/com/findoutmyloan/application/customer/service/impl/CustomerServiceImplTest.java
+++ src/test/java/com/findoutmyloan/application/customer/service/impl/CustomerServiceImplTest.java
@@ -5,7 +5,6 @@ import com.findoutmyloan.application.customer.dto.CustomerResponseDTO;
 import com.findoutmyloan.application.customer.dto.CustomerSaveRequestDTO;
 import com.findoutmyloan.application.customer.dto.CustomerUpdateRequestDTO;
 import com.findoutmyloan.application.customer.entity.Customer;
-import com.findoutmyloan.application.customer.enums.CustomerTypeAccordingToMonthlyIncome;
 import com.findoutmyloan.application.customer.repository.CustomerRepository;
 import com.findoutmyloan.application.customer.validation.CustomerValidationService;
 import com.findoutmyloan.application.general.exception.ItemNotFoundException;
@@ -19,6 +18,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDate;
@@ -36,23 +37,35 @@ class CustomerServiceImplTest {
     @Mock
     Customer customer;
     @Mock
-    AuthenticationService authenticationService;
+    private AuthenticationService authenticationService;
     @InjectMocks
     private CustomerServiceImpl customerService;
-    @Mock
-    private LoanService loanService;
-
-    @Mock
-    private CustomerValidationService customerValidationService;
     @Mock
     private PasswordEncoder passwordEncoder;
     @Mock
     private CustomerRepository customerRepository;
-
+    @Mock
+    private LoanService loanService;
+@Mock
+CustomerValidationService customerValidationService;
 
     @Test
-    void shouldGetLimitOfCustomer() {
+    void shouldGetUpdatedLimitOfCustomer() {
 
+        Authentication authentication=mock(Authentication.class);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        long identityNo=81655500404L;
+        Customer customer=mock(Customer.class);
+        when(customer.getIdentityNo()).thenReturn(identityNo);
+
+        when(authenticationService.getCurrentCustomer()).thenReturn(customer);
+        when(customer.getCustomerLimit()).thenReturn(1000f);
+
+        when(customerRepository.findByIdentityNo(identityNo)).thenReturn(Optional.of(customer));
+        float updatedLimit=customerService.getUpdatedLimitOfCustomer(500.0f);
+
+        assertEquals(1500.0f, updatedLimit);
     }
 
     @Test
@@ -77,17 +90,6 @@ class CustomerServiceImplTest {
     @Test
     void shouldThrowNullPointerExceptionWhenSaveCustomerParameterIsNull() {
         assertThrows(NullPointerException.class, ()->customerService.saveCustomer(null));
-    }
-
-    @Test
-    void getCustomerTypeAccordingToMonthlyIncome() {
-        long identityNo=81655500404L;
-        Customer customer=mock(Customer.class);
-        when(customer.getIdentityNo()).thenReturn(identityNo);
-        when(customer.getPersonType()).thenReturn(PersonType.CUSTOMER);
-        when(customerRepository.findByIdentityNo(identityNo)).thenReturn(Optional.of(customer));
-        CustomerTypeAccordingToMonthlyIncome result=customerService.getCustomerTypeAccordingToMonthlyIncome(identityNo);
-
     }
 
     @Test
