ed51ceb5bd5e71cdc921e879478281756d00b9a4
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-22 21:04:06 +0300

    REF: loan validation

--- src/main/java/com/findoutmyloan/application/loan/validation/LoanValidationService.java
+++ src/main/java/com/findoutmyloan/application/loan/validation/LoanValidationService.java
@@ -1,9 +1,11 @@
 package com.findoutmyloan.application.loan.validation;
 /* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
 
+import com.findoutmyloan.application.customer.enums.CustomerProfiler;
 import com.findoutmyloan.application.loan.entity.Loan;
 
 public interface LoanValidationService {
     void validateLoan(Loan loan);
-
+    void validateCreditScore(int creditScore);
+    void validateCustomerProfile(CustomerProfiler customerProfiler);
 }
