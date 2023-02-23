389d9233693a90a7b8d5d85127e0bc8ecf4ed0ef
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-23 23:51:56 +0300

    REF: rearrange code

--- src/main/java/com/findoutmyloan/application/security/service/AuthenticationService.java
+++ src/main/java/com/findoutmyloan/application/security/service/AuthenticationService.java
@@ -1,26 +1,15 @@
 package com.findoutmyloan.application.security.service;
 /* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
 
-import com.findoutmyloan.application.customer.dto.CustomerDTO;
 import com.findoutmyloan.application.customer.dto.CustomerResponseDTO;
 import com.findoutmyloan.application.customer.dto.CustomerSaveRequestDTO;
 import com.findoutmyloan.application.customer.entity.Customer;
-import com.findoutmyloan.application.customer.mapper.CustomerMapper;
-import com.findoutmyloan.application.customer.service.CustomerService;
 import com.findoutmyloan.application.security.dto.SecurityLoginRequestDTO;
-import com.findoutmyloan.application.security.enums.JwtConstant;
-import com.findoutmyloan.application.security.jwt.JwtTokenGenerator;
-import com.findoutmyloan.application.security.jwt.JwtUserDetails;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 public interface AuthenticationService {
-     CustomerResponseDTO register(CustomerSaveRequestDTO customerSaveRequestDTO);
-     String login(SecurityLoginRequestDTO securityLoginRequestDTO);
-     Customer getCurrentCustomer();
+    CustomerResponseDTO register(CustomerSaveRequestDTO customerSaveRequestDTO);
+
+    String login(SecurityLoginRequestDTO securityLoginRequestDTO);
+
+    Customer getCurrentCustomer();
 }
