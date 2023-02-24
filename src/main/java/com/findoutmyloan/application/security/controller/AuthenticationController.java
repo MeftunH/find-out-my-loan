a5009fdf607fe3e0d92e6a45f145b46dea4e11d5
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-24 20:06:31 +0300

    DOC: add example object

--- src/main/java/com/findoutmyloan/application/security/controller/AuthenticationController.java
+++ src/main/java/com/findoutmyloan/application/security/controller/AuthenticationController.java
@@ -7,6 +7,7 @@ import com.findoutmyloan.application.generic.dto.RestResponse;
 import com.findoutmyloan.application.security.dto.SecurityLoginRequestDTO;
 import com.findoutmyloan.application.security.service.AuthenticationService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,14 +21,63 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
-    @Operation(tags="Authentication", summary = "Login", description = "Login with identity no and password")
+    @Operation(tags = "Authentication", summary = "Login", description = "Login with identity no and password",
+            requestBody= @io.swagger.v3.oas.annotations.parameters.RequestBody
+            (
+                    content = @io.swagger.v3.oas.annotations.media.
+                            Content(
+                            mediaType = "application/json",
+                            schema = @io.swagger.v3.oas.annotations.media.
+                                    Schema(implementation = SecurityLoginRequestDTO.class),
+                            examples = {
+                                    @ExampleObject(
+                                            name = "Login credentials",
+                                            summary = "For jwt token",
+                                            description = "identityNo and password",
+                                            value = "{\n"+
+                                                    "\t \"identityNo\":89565435274,\n"+
+                                                    "\t\t\"password\": \"123MmM\"\n"+
+                                                    "}"
+                                    )
+                            }
+                    )
+            )
+    )
     @PostMapping("/login")
     public ResponseEntity<RestResponse<String>> login(@RequestBody SecurityLoginRequestDTO securityLoginRequestDTO) {
         String token=authenticationService.login(securityLoginRequestDTO);
         return ResponseEntity.ok(RestResponse.of(token));
     }
 
-    @Operation(tags="Authentication", summary = "Register", description = "Register")
+    @Operation(tags = "Authentication", summary = "Register", description = "Register",
+            requestBody= @io.swagger.v3.oas.annotations.parameters.RequestBody
+                    (
+                            content = @io.swagger.v3.oas.annotations.media.
+                                    Content(
+                                    mediaType = "application/json",
+                                    schema = @io.swagger.v3.oas.annotations.media.
+                                            Schema(implementation = CustomerSaveRequestDTO.class),
+                                    examples = {
+                                            @ExampleObject(
+                                                    name = "new user",
+                                                    summary = "New User Example",
+                                                    description = "Complete request with all available fields for user",
+                                                    value = "{\n"+
+                                                            "\t\"name\": \"John\",\n"+
+                                                            "  \"surname\": \"Doe\",\n"+
+                                                            "  \"identityNo\": 16079248434,\n"+
+                                                            "\"birthDate\": \"1999-01-01\",   \n"+
+                                                            "  \"phoneNumber\": \"5655555556\",\n"+
+                                                            "  \"personType\": \"CUSTOMER\",\n"+
+                                                            "  \"monthlyIncome\": 5000.0,\n"+
+                                                            "  \"paybackGuaranteeType\": \"COLLATERAL\",\n"+
+                                                            "\t\"password\": \"123MmM\"\n"+
+                                                            "}"
+                                            )
+                                    }
+                            )
+                    )
+    )
     @PostMapping("/register")
     public ResponseEntity<RestResponse<CustomerResponseDTO>> register(@RequestBody CustomerSaveRequestDTO customerSaveRequestDTO) {
         CustomerResponseDTO customerResponseDTO=authenticationService.register(customerSaveRequestDTO);
