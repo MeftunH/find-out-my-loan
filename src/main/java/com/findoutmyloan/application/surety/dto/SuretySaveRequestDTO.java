b2cacf1677bfbcce378c4a6d3b2eed3b5dc6dcb6
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-24 13:17:21 +0300

    BUGFIX: type definition error

--- src/main/java/com/findoutmyloan/application/surety/dto/SuretySaveRequestDTO.java
+++ src/main/java/com/findoutmyloan/application/surety/dto/SuretySaveRequestDTO.java
@@ -3,9 +3,7 @@ package com.findoutmyloan.application.surety.dto;
 import com.findoutmyloan.application.person.dto.PersonDTO;
 import com.findoutmyloan.application.surety.entity.Surety;
 import com.findoutmyloan.application.surety.enums.SuretyType;
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 import java.io.Serializable;
@@ -16,9 +14,11 @@ import java.util.Date;
  */
 @EqualsAndHashCode(callSuper = true)
 @Data
-@SuperBuilder
+@SuperBuilder(toBuilder = true)
+@AllArgsConstructor(access = AccessLevel.PRIVATE) // type definition error solved
 public class SuretySaveRequestDTO extends PersonDTO implements Serializable {
     private SuretyType suretyType;
     private Date baseAdditionalFieldsCreatedDate;
     private Date baseAdditionalFieldsUpdatedDate;
+
 }
